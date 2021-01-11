26515b7985594f780381da86e061a4d11af94704
francofonceca <francofonceca15@gmail.com>
2021-01-11 17:20:08 -0300

    agregando redireccion correctamente hacia la aplicacion real

--- src/main/java/com/arcor/springsecurityldap/controller/LoginController.java
+++ src/main/java/com/arcor/springsecurityldap/controller/LoginController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
@@ -16,14 +18,21 @@ public class LoginController {
         return "login";
     }
     
+    // @GetMapping("/")
+    // public String start() {
+    //     return "home";
+    // }
     @GetMapping("/")
-    public String start() {
-        return "home";
-    }
+    public ModelAndView method() {
+            String projectUrl = "https://idpa.webaccess.arcor.com/nidp/portal?locale=es_ES";
+            return new ModelAndView("redirect:" + projectUrl);
 
+    }
     
     @GetMapping("/home")
     public String home() {
         return "home";
     }
+
+    
 }
