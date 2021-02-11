e12b95d3f6f2759858ea50d04f03853c0d1db696
Doval German <german.doval@gmail.com>
2021-02-11 17:14:46 -0300

    Test CORS

--- spring-security-ldap/src/main/java/com/arcor/springsecurityldap/SpringSecurityLdapApplication.java
+++ spring-security-ldap/src/main/java/com/arcor/springsecurityldap/SpringSecurityLdapApplication.java
@@ -9,5 +9,5 @@ public class SpringSecurityLdapApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityLdapApplication.class, args);
 	}
-
+	
 }
--- spring-security-ldap/src/main/java/com/arcor/springsecurityldap/security/SecurityConfiguration.java
+++ spring-security-ldap/src/main/java/com/arcor/springsecurityldap/security/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package com.arcor.springsecurityldap.security;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -12,6 +16,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.ldap.userdetails.UserDetailsContextMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -43,7 +50,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Override
     protected void configure(HttpSecurity http) throws Exception{
         
-		http.httpBasic().and().authorizeRequests().anyRequest().authenticated().and().csrf().disable();
+		http.httpBasic()
+		.and()
+		.authorizeRequests()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.cors()
+		.and()
+		.csrf()
+		.disable();
 		
 		/*http.authorizeRequests()
                 .anyRequest().fullyAuthenticated()
@@ -51,6 +67,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
                 .formLogin()
                 .loginPage("/login")
                 .permitAll();*/
+    }
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(new ArrayList<>(Arrays.asList("*")));
+        configuration.setAllowedMethods(new ArrayList<>(Arrays.asList("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH")));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(new ArrayList<>(Arrays.asList("Authorization", "Cache-Control", "Content-Type")));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 	
 	 
